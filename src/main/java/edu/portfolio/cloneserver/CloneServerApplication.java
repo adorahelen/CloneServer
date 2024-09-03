0bdaff8099687ef81709915f0a4afa9bf3b700d9
adorahelen <adorahelenmin@gmail.com>
2024-09-03 09:24:03 +0900

    Initial commit

---
+++ src/main/java/edu/portfolio/cloneserver/CloneServerApplication.java
@@ -0,0 +1,13 @@
+package edu.portfolio.cloneserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/portfolio/cloneserver/CloneServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.portfolio.cloneserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
