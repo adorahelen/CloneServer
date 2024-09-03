e9a78cf4957a048078b212a6a3a25b215279916b
adorahelen <adorahelenmin@gmail.com>
2024-09-03 09:31:01 +0900

    바뀐게 없는데..?

--- src/main/java/edu/portfolio/cloneserver/CloneServerApplication.java
+++ src/main/java/edu/portfolio/cloneserver/CloneServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CloneServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CloneServerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(CloneServerApplication.class, args);
+	}
 
 }
